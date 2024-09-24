f393a798d4eecc9e88cf532f134dc6cde23c2019
Theo Penha Valente <theopvalente@gmail.com>
2024-09-24 11:31:40 -0300

    ----

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ function effectFade() {
     myMessage.style.opacity = "1";  
     myMessage.classList.remove('fade-out');   
     setTimeout(() => myMessage.classList.add('fade-out'), 1500);
-}
+};
 
 button.addEventListener("click", effectFade);
 
 
 if (window.getComputedStyle(myMessage).getPropertyValue('opacity') == 0) {
     effectFade(); 
-}
+};
 
+
+
+let count = 0;
+
+document.getElementById('myButton').addEventListener('click', () => {
+    count++;
+    document.getElementById('key').textContent = String(count).padStart(3, '0');
+    document.getElementById('counter').style.display = 'block';    
+});
